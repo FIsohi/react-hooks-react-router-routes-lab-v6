11444621fa763a7f3718246a003632587892cae3
+ <+>
2024-06-10 11:53:15 +0300

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import "./index.css";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RouterProvider } from 'react-router-dom';
+import routes from './routes';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+ReactDOM.render(
+    <RouterProvider router={routes} />,
+    document.getElementById('root')
+);
